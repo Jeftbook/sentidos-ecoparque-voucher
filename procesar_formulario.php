cfddb7f28d231eb056a456659d577247c681ca89
Jeftbook <109643021+Jeftbook@users.noreply.github.com>
2024-12-16 20:48:37 -0300

    Algo7

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -4,6 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nombre = $_POST['nombre'];
   $apellido = $_POST['apellido'];
   $mail = $_POST['mail'];
+  $codArea = $_POST['codArea'];
   $telefono = $_POST['telefono'];
   $provincia = $_POST['provincia'];
   $localidad = $_POST['localidad'];
@@ -16,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "Nombre: $nombre<br>";
   echo "Apellido: $apellido<br>";
   echo "Mail: $mail<br>";
-  echo "Telefono: $telefono<br>";
+  echo "Telefono: $codArea + $telefono<br>";
   echo "Provincia: $provincia<br>";
   echo "Localidad: $localidad<br>";
   echo "Cuantos van: $cuantos_van<br>";
