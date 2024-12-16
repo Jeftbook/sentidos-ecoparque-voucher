3b7753a5c1f7cb4230149e747bba22fe98205114
Jeftbook <109643021+Jeftbook@users.noreply.github.com>
2024-12-16 20:22:18 -0300

    Algo4

--- index.php
+++ index.php
@@ -13,38 +13,43 @@
   <form action="procesar_formulario.php" method="post">
     <div class="mb-3">
       <label for="nombre" class="form-label">Nombre</label>
-      <input type="text" class="form-control" id="nombre" aria-describedby="emailHelp">
-    </div>
-
-    <div class="mb-3">
-      <label for="apellido" class="form-label">Apellido</label>
-      <input type="text" class="form-control" id="apellido" aria-describedby="emailHelp">
-    </div>
-
-    <!-- <div class="mb-3">
-      <label for="nombre" class="form-label">Nombre:</label>
       <input type="text" class="form-control" id="nombre" name="nombre" required>
     </div>
 
     <div class="mb-3">
-      <label for="apellido" class="form-label">Apellido:</label>
+      <label for="apellido" class="form-label">Apellido</label>
       <input type="text" class="form-control" id="apellido" name="apellido" required>
     </div>
 
     <div class="mb-3">
-      <label for="dni" class="form-label">DNI:</label>
-      <input type="number" class="form-control" id="dni" name="dni" required>
+      <label for="provincia" class="form-label">Provincia</label>
+      <input type="text" class="form-control" id="provincia" name="provincia" required>
     </div>
 
     <div class="mb-3">
-      <label for="cantidad_personas" class="form-label">Cantidad de Personas:</label>
-      <input type="number" class="form-control" id="cantidad_personas" name="cantidad_personas" min="1" required>
+      <label for="localidad" class="form-label">Localidad</label>
+      <input type="text" class="form-control" id="localidad" name="localidad" required>
     </div>
 
     <div class="mb-3">
-      <label for="fecha" class="form-label">Fecha:</label>
-      <input type="date" class="form-control" id="fecha" name="fecha" required>
-    </div> -->
+      <label for="cuantos_van" class="form-label">¿Cuantos van?</label>
+      <input type="number" class="form-control" id="cuantos_van" name="cuantos_van" min="1" max="10" required>
+    </div>
+
+    <div class="mb-3">
+      <label for="fecha" class="form-label">¿Que dia vas a asistir?</label>
+      <input type="date" class="form-control" id="fecha" name="fecha" max="2025-03-31" required>
+    </div>
+
+    <div class="mb-3">
+      <label for="mail" class="form-label">Mail</label>
+      <input type="text" class="form-control" id="mail" name="mail" required>
+    </div>
+
+    <div class="mb-3">
+      <label for="telefono" class="form-label">Número de Telefono</label>
+      <input type="number" class="form-control" id="telefono" name="telefono" required>
+    </div>
 
     <button type="submit" class="btn btn-primary">Enviar</button>
   </form>
